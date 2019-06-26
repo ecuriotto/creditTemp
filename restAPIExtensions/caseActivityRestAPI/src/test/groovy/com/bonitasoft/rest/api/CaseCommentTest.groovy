ff240e6e843a539fcef6b3b239153acd39131c79
Romain Bioteau <romain.bioteau@bonitasoft.com>
2019-06-26 17:03:40 +0200

    add CaseComment unit tests

---
+++ restAPIExtensions/caseActivityRestAPI/src/test/groovy/com/bonitasoft/rest/api/CaseCommentTest.groovy
@@ -0,0 +1,167 @@
+package com.bonitasoft.rest.api
+
+import java.time.LocalDateTime
+
+import javax.servlet.http.HttpServletRequest
+
+import com.bonitasoft.engine.api.APIClient
+import com.bonitasoft.engine.api.IdentityAPI
+import com.bonitasoft.engine.api.ProcessAPI
+import com.bonitasoft.rest.api.Case
+import com.bonitasoft.web.extension.rest.RestAPIContext
+
+import groovy.json.JsonBuilder
+import groovy.json.JsonSlurper
+
+import org.bonitasoft.engine.bpm.comment.Comment
+import org.bonitasoft.engine.bpm.comment.SearchCommentsDescriptor
+import org.bonitasoft.engine.bpm.data.DataInstance
+import org.bonitasoft.engine.bpm.flownode.ActivityInstance
+import org.bonitasoft.engine.bpm.flownode.ArchivedActivityInstanceSearchDescriptor
+import org.bonitasoft.engine.bpm.flownode.ArchivedHumanTaskInstance
+import org.bonitasoft.engine.bpm.flownode.FlowNodeType
+import org.bonitasoft.engine.bpm.flownode.HumanTaskInstance
+import org.bonitasoft.engine.bpm.flownode.impl.internal.ActivityInstanceImpl
+import org.bonitasoft.engine.bpm.flownode.impl.internal.HumanTaskInstanceImpl
+import org.bonitasoft.engine.bpm.process.ProcessDefinition
+import org.bonitasoft.engine.bpm.process.ProcessInstance
+import org.bonitasoft.engine.bpm.process.impl.internal.ProcessDefinitionImpl
+import org.bonitasoft.engine.identity.User
+import org.bonitasoft.engine.search.Order
+import org.bonitasoft.engine.search.SearchFilterOperation
+import org.bonitasoft.engine.search.SearchOptions
+import org.bonitasoft.engine.search.SearchResult
+import org.bonitasoft.engine.search.impl.SearchFilter
+import org.bonitasoft.engine.search.impl.SearchOptionsImpl
+import org.bonitasoft.web.extension.rest.RestApiResponse
+import org.bonitasoft.web.extension.rest.RestApiResponseBuilder
+import spock.lang.Specification
+
+class CaseCommentTest extends Specification {
+
+    ProcessAPI processAPI = Mock()
+	IdentityAPI identityAPI = Mock()
+    APIClient apiClient = Mock()
+    HttpServletRequest request = Mock()
+    RestAPIContext context = Mock()
+    SearchResult<ProcessInstance> result = Mock()
+    SearchResult<HumanTaskInstance> taskResult = Mock()
+    DataInstance dataInstance = Mock()
+	SearchResult EMPTY_RESULT =  Mock()
+
+    def "setup"() {
+        context.apiClient >> apiClient
+        apiClient.getProcessAPI() >> processAPI
+		apiClient.getIdentityAPI() >> identityAPI
+    }
+
+    def "should return a bad request code when no caseId parameter is found"() {
+        given:
+        def caseComment = new CaseComment()
+
+		
+        when:
+        RestApiResponse restApiResponse = caseComment.doHandle(request, new RestApiResponseBuilder() , context)
+
+        then:
+		assert restApiResponse.httpStatus == 400
+		assert new JsonSlurper().parseText(restApiResponse.response).error == 'the parameter caseId is missing'
+    }
+	
+	def "should query case comments with proper search options"() {
+		given:
+		def caseComment = new CaseComment()
+		def SearchResult commentsResult = Mock()
+		
+		when:
+		request.getParameter('caseId') >> 1L
+		RestApiResponse restApiResponse = caseComment.doHandle(request, new RestApiResponseBuilder() , context)
+
+		then:
+		1 * processAPI.searchComments({ SearchOptions searchOptions ->
+			
+			assert searchOptions.filters[0].field == SearchCommentsDescriptor.PROCESS_INSTANCE_ID
+			assert searchOptions.filters[0].value == 1L
+			
+			assert searchOptions.sorts[0].field == SearchCommentsDescriptor.POSTDATE
+			assert searchOptions.sorts[0].order == Order.DESC
+			
+			searchOptions
+				
+		}) >> commentsResult
+	}
+	
+	def "should return the list of comments for a given case id"() {
+		given:
+		def caseComment = new CaseComment()
+		def SearchResult commentsResult = Mock()
+		def c1 = Stub(Comment){
+			it.content >> 'Hello'
+			it.postDate >> Date.parse("yyyy-MM-dd hh:mm:ss", "2019-04-03 1:23:45").time
+			it.userId >> 4L
+		}
+		def c2 = Stub(Comment){
+			it.content >> 'Hello\nWorld'
+			it.postDate >> Date.parse("yyyy-MM-dd hh:mm:ss", "2019-04-06 4:23:45").time
+			it.userId >> 5L
+		}
+		commentsResult.result >> [c1,c2]
+		processAPI.searchComments(_) >> commentsResult
+		def walter = Stub(User){
+			it.id >> 4L
+			it.firstName >> 'Walter'
+			it.lastName >> 'Bates'
+			it.username >> 'walter.bates'
+		}
+		def helen = Stub(User){
+			it.id >> 5L
+			it.firstName >> 'Helen'
+			it.lastName >> 'Kelly'
+			it.username >> 'helen.kelly'
+		}
+		identityAPI.getUser(4L) >> walter
+		identityAPI.getUser(5L) >> helen
+		
+		
+		when:
+		request.getParameter('caseId') >> 1L
+		RestApiResponse restApiResponse = caseComment.doHandle(request, new RestApiResponseBuilder() , context)
+
+		then:
+		assert restApiResponse.httpStatus == 200
+		def comments = new JsonSlurper().parseText(restApiResponse.response)
+		assert comments.size() == 2
+		
+		assert comments[0].content == 'Hello'
+		assert comments[0].postDate == Date.parse("yyyy-MM-dd hh:mm:ss", "2019-04-03 1:23:45").time
+		assert comments[0].username == 'Walter Bates'
+		
+		assert comments[1].content == 'Hello<br>World'
+		assert comments[1].postDate == Date.parse("yyyy-MM-dd hh:mm:ss", "2019-04-06 4:23:45").time
+		assert comments[1].username == 'Helen Kelly'
+	}
+	
+	def "should filter system comments"() {
+		given:
+		def caseComment = new CaseComment()
+		def SearchResult commentsResult = Mock()
+		def systemComment = Stub(Comment){
+			it.content >> 'Task finished'
+			it.postDate >> Date.parse("yyyy-MM-dd hh:mm:ss", "2019-04-03 1:23:45").time
+			it.userId >> -1
+		}
+		commentsResult.result >> [systemComment]
+		processAPI.searchComments(_) >> commentsResult
+		
+		
+		when:
+		request.getParameter('caseId') >> 1L
+		RestApiResponse restApiResponse = caseComment.doHandle(request, new RestApiResponseBuilder() , context)
+
+		then:
+		assert restApiResponse.httpStatus == 200
+		def comments = new JsonSlurper().parseText(restApiResponse.response)
+		assert comments.size() == 0
+	}
+	
+}
