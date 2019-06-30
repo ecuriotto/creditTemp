eb0eb93c628968b946832a839aa7d25970884000
Romain Bioteau <romain.bioteau@bonitasoft.com>
2019-06-30 11:36:30 +0200

    new timeline widget (bootstrap friendly) improve customer info fragment

--- web_widgets/customTimeline/customTimeline.js
+++ web_widgets/customTimeline/customTimeline.js
@@ -6,6 +6,6 @@
   }
 })().directive('customTimeline', function() {
     return {
-      template: ' <div class="well well-sm">\n     <div class="timeline">\n          <div ng-class="$index % 2 === 0 ? \'box left\' : \'box right\'" ng-repeat="event in properties.events track by $index">\n            <div class="content">\n              <h3 ng-bind-html="event.title"></h3>\n              <p ng-bind-html="event.body"></p>\n            </div>\n          </div>\n    </div>\n</div>'
+      template: '    <ul class="timeline">\n        <li ng-class="$index % 2 === 0 ? \'\' : \'timeline-inverted\'" ng-repeat="event in properties.events track by $index">\n            <div class="timeline-badge"><i class="glyphicon glyphicon-check"></i></div>\n             <div class="timeline-panel">\n                  <div class="timeline-heading">\n                      <h4 class="timeline-title" ng-bind-html="event.title"></h4>\n                      <p><small class="text-muted"><i class="glyphicon glyphicon-user"></i> {{event.user}}</small> | <small class="text-muted"><i class="glyphicon glyphicon-time"></i> {{event.time}}</small></p>\n                  </div>\n                  <div class="timeline-body">\n                      <p ng-bind-html="event.body"></p>\n                  </div>\n              </div>\n        </li>\n    </ul>'
     };
   });
