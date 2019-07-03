6ebbbb1f498301185fb011133c7e496394a8644f
Romain Bioteau <romain.bioteau@bonitasoft.com>
2019-07-03 19:41:40 +0200

    Create Dispute from Case list

--- restAPIExtensions/creditCardDisputeResolutionRestAPI/src/main/groovy/com/company/dispute/api/Case.groovy
+++ restAPIExtensions/creditCardDisputeResolutionRestAPI/src/main/groovy/com/company/dispute/api/Case.groovy
@@ -1,5 +1,8 @@
 package com.company.dispute.api
 
+import java.time.OffsetDateTime
+import java.time.format.DateTimeFormatter
+
 import javax.servlet.http.HttpServletRequest
 import javax.servlet.http.HttpServletResponse
 
@@ -7,6 +10,7 @@ import com.bonitasoft.engine.api.ProcessAPI
 import com.bonitasoft.engine.bpm.process.impl.ProcessInstanceSearchDescriptor
 import com.bonitasoft.web.extension.rest.RestAPIContext
 import com.bonitasoft.web.extension.rest.RestApiController
+import com.company.model.CustomerDAO
 import com.company.model.DisputeDAO
 
 import groovy.json.JsonBuilder
@@ -33,20 +37,32 @@ class Case implements RestApiController, CaseActivityHelper, BPMNamesConstants{
 				.done()
 		def result = processAPI.searchProcessInstances(searchOptions).getResult()
 				.collect {
+					def dispute = searchData.search(it.id,'dispute_ref',context.getApiClient().getDAO(DisputeDAO))
+					def customer = searchData.search(it.id,'customer_ref',context.getApiClient().getDAO(CustomerDAO))
 					[
 						id: it.id,
-						status: searchData.search(it.id,'dispute_ref',context.getApiClient().getDAO(DisputeDAO))?.status,
+						status: dispute?.status,
+						merchantIdNumber: dispute?.merchantIdNumber,
 						url: viewActionLink(it.id, processAPI, contextPath),
-						target:'_self'
+						target:'_target',
+						customer: "$customer.firstName $customer.lastName",
+						lastUpdateDate:dispute?.lastUpdateDate?.format(DateTimeFormatter.ISO_OFFSET_DATE_TIME),
+						open: true
 					]
 				}
 		processAPI.searchArchivedProcessInstances(searchOptions).getResult()
 				.collect {
+					def dispute = searchData.search(it.id,'dispute_ref',context.getApiClient().getDAO(DisputeDAO))
+					def customer = searchData.search(it.id,'customer_ref',context.getApiClient().getDAO(CustomerDAO))
 					result << [
 						id: it.id,
-						status: searchData.search(it.id,'dispute_ref',context.getApiClient().getDAO(DisputeDAO))?.status,
+						status: dispute?.status,
+						merchantIdNumber: dispute?.merchantIdNumber,
 						url: viewActionLink(it.id, processAPI, contextPath),
-						target:'_self'
+						target:'_self',
+						customer: "$customer.firstName $customer.lastName",
+						lastUpdateDate:dispute?.lastUpdateDate?.format(DateTimeFormatter.ISO_OFFSET_DATE_TIME),
+						open: false
 					]
 				}
 
@@ -56,6 +72,7 @@ class Case implements RestApiController, CaseActivityHelper, BPMNamesConstants{
 			build()
 		}
 	}
+	
 	def SearchBusinessData newSearchBusinessData(ProcessAPI processAPI) {
 		new SearchBusinessData(processAPI)
 	}
@@ -63,7 +80,7 @@ class Case implements RestApiController, CaseActivityHelper, BPMNamesConstants{
 	def String viewActionLink(long caseId, ProcessAPI processAPI, contextPath) {
 		try {
 			def instance = processAPI.getProcessInstance(caseId)
-			instance ? "../../case/content/?id=$caseId" : ''
+			instance ? "$contextPath/apps/cases/case?id=$caseId" : ''
 		}catch(ProcessInstanceNotFoundException e) {
 			def instance = processAPI.getArchivedProcessInstance(caseId)
 			def pDef = processAPI.getProcessDefinition(instance.processDefinitionId)
