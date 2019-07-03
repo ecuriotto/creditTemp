ed5c0d5392225e7c6e205afd9c0ddb4be6c7fe79
Romain Bioteau <romain.bioteau@bonitasoft.com>
2019-07-03 09:13:23 +0200

    rename project

---
+++ src-groovy/com/company/model/DisputeStates.groovy
@@ -0,0 +1,11 @@
+package com.company.model
+
+class DisputeStates {
+	
+	def static PENDING = 'PENDING'
+	def static INVALID = 'INVALID'
+	def static INVESTIGATING = 'INVESTIGATING'
+	def static PROCESSING_CHARGEBACK = 'PROCESSING CHARGEBACK'
+	def static RESOLVED = 'PROCESSING CHARGEBACK'
+	
+}
