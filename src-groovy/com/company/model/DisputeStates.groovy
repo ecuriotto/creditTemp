d48e59b9f675967a2a79516f978b466a77ed65d7
Alejandro Rondón <45388095+AleRondon@users.noreply.github.com>
2019-10-09 16:30:49 +0200

    Disable update dispute after chargeback (#25)
    * New Dispute Status Created "Charged Back"
    * Disable update when dispute status is "Charged Back"

--- src-groovy/com/company/model/DisputeStates.groovy
+++ src-groovy/com/company/model/DisputeStates.groovy
@@ -6,6 +6,7 @@ class DisputeStates {
 	def static final INVALID = 'INVALID'
 	def static final INVESTIGATING = 'INVESTIGATING'
 	def static final PROCESSING_CHARGEBACK = 'PROCESSING CHARGEBACK'
+	def static final CHARGED_BACK = 'CHARGED BACK'
 	def static final RESOLVED = 'RESOLVED'
 	
 }
